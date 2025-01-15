d2c74c23af39a3453ec77ab93e8db08b9359488e
Garoui <118277240+Garoui@users.noreply.github.com>
2025-01-15 13:04:58 +0100

    afiiche list etudiant

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import Hello from './Composant/Hello';
 function App() {
+  //javascript
+  //1
+  const name = "abir karoui"
+  const element = <h1>Hello</h1>
+  //2
+  const etudiant = {
+    nom: "ahmed", prenom: "ben foulen", age: 10,
+  };
+  //3
+  const listetudiant = [
+    { nom: "yassin", prenom: "Agrbaoui", age: 20, id: 0 },
+    { nom: "abir", prenom: "karoui", age: 24, id: 1 },
+    { nom: "aziz", prenom: "ben foulen", age: 10, id: 2 },
+    { nom: "yomna", prenom: "agrbaoui", age: 1, id: 3 },
+  ];
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {name}
+        {element}
+        {etudiant.nom} | {etudiant.prenom} | {etudiant.age}
+        <br />
+        {listetudiant.map(function (item) {
+          return <div key={item.id}>{item.nom} |{item.prenom} |{item.age} </div>
+        }
+        )}
+        function
+        <Hello abir="karoui"></Hello>
       </header>
     </div>
   );
